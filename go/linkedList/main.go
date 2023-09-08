ea9544e3dd65ae8fd9d86b726336423591b4ad04
a.o.varfolomeev <a.o.varfolomeev@tinkoff.ru>
2023-09-08 18:49:36 +0300

    Initial commit

---
+++ go/linkedList/main.go
@@ -0,0 +1,137 @@
+package main
+
+import "errors"
+
+type Node struct {
+	next  *Node
+	value int
+}
+
+type LinkedList struct {
+	head *Node
+	tail *Node
+}
+
+func (l *LinkedList) AddInTail(item Node) {
+	if l.head == nil {
+		l.head = &item
+	} else {
+		l.tail.next = &item
+	}
+
+	l.tail = &item
+}
+
+func (l *LinkedList) Count() int {
+	cnt := 0
+
+	node := l.head
+	for node != nil {
+		cnt++
+		node = node.next
+	}
+
+	return cnt
+}
+
+func (l *LinkedList) Find(n int) (Node, error) {
+	node := l.head
+	for node != nil {
+		if node.value == n {
+			return *node, nil
+		}
+		node = node.next
+	}
+
+	return Node{}, errors.New("node not found")
+}
+
+func (l *LinkedList) FindAll(n int) []Node {
+	var nodes []Node
+
+	node := l.head
+	for node != nil {
+		if node.value == n {
+			nodes = append(nodes, *node)
+		}
+		node = node.next
+	}
+
+	return nodes
+}
+
+func (l *LinkedList) Delete(n int, all bool) {
+	deleted := false
+
+	for l.head != nil && l.head.value == n {
+		l.head = l.head.next
+		if !all {
+			deleted = true
+			break
+		}
+	}
+
+	if l.head == nil {
+		l.tail = nil
+		return
+	}
+
+	if deleted {
+		return
+	}
+
+	for node := l.head; node != nil && node.next != nil; node = node.next {
+		if node.next.value == n {
+			if node.next == l.tail {
+				l.tail = node
+			}
+
+			node.next = node.next.next
+			if !all {
+				return
+			}
+		}
+	}
+}
+
+func (l *LinkedList) deleteHead(n int, all bool) {
+	for l.head != nil && l.head.value == n {
+		l.head = l.head.next
+		if !all {
+			if l.head == nil {
+				l.tail = nil
+			}
+			return
+		}
+	}
+}
+
+func (l *LinkedList) Insert(after *Node, add Node) {
+	node := l.head
+	for node != nil {
+		if node == after {
+			add.next = node.next
+			node.next = &add
+
+			if node == l.tail {
+				l.tail = &add
+			}
+			return
+		}
+		node = node.next
+	}
+}
+
+func (l *LinkedList) InsertFirst(first Node) {
+	first.next = l.head
+	l.head = &first
+
+	if l.head.next == nil {
+		l.tail = &first
+	}
+}
+
+func (l *LinkedList) Clean() {
+	l.head = nil
+	l.tail = nil
+}
---
+++ go/linkedList/main_test.go
@@ -0,0 +1,322 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLinkedList_AddInTail_EmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	l.AddInTail(Node{value: 12})
+
+	assert.NotNil(t, l.tail)
+	assert.NotNil(t, l.head)
+	assert.Equal(t, 1, l.Count())
+	assert.Equal(t, 12, l.head.value)
+	assert.Equal(t, 12, l.tail.value)
+}
+
+func TestLinkedList_AddInTail_NotEmptyList(t *testing.T) {
+	l := LinkedList{}
+	l.AddInTail(Node{value: 1})
+	l.AddInTail(Node{value: 2})
+	l.AddInTail(Node{value: 12})
+
+	assert.NotNil(t, l.tail)
+	assert.NotNil(t, l.head)
+	assert.Equal(t, 3, l.Count())
+	assert.Equal(t, 1, l.head.value)
+	assert.Equal(t, 12, l.tail.value)
+}
+
+func TestLinkedList_Count_EmptyList(t *testing.T) {
+	l := LinkedList{}
+	c := l.Count()
+
+	assert.Equal(t, 0, c)
+}
+
+func TestLinkedList_Count_NotEmptyList(t *testing.T) {
+	l := LinkedList{}
+	l.AddInTail(Node{value: 1})
+	l.AddInTail(Node{value: 2})
+	l.AddInTail(Node{value: 3})
+
+	c := l.Count()
+
+	assert.Equal(t, 3, c)
+}
+
+func TestLinkedList_Count_NotEmptyListAfterDeleteElement(t *testing.T) {
+	l := LinkedList{}
+	l.AddInTail(Node{value: 1})
+	l.AddInTail(Node{value: 2})
+	l.AddInTail(Node{value: 3})
+
+	l.Delete(2, false)
+	c := l.Count()
+
+	assert.Equal(t, 2, c)
+}
+
+func TestLinkedList_Count_EmptyListAfterDeleteAllElements(t *testing.T) {
+	l := LinkedList{}
+	l.AddInTail(Node{value: 2})
+	l.AddInTail(Node{value: 2})
+	l.AddInTail(Node{value: 2})
+
+	l.Delete(2, true)
+	c := l.Count()
+
+	assert.Equal(t, 0, c)
+}
+
+func TestLinkedList_Find_EmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	find, err := l.Find(2)
+
+	assert.Error(t, err)
+	assert.Equal(t, Node{}, find)
+}
+
+func TestLinkedList_Find_NotFound(t *testing.T) {
+	l := LinkedList{}
+	l.AddInTail(Node{value: 1})
+	l.AddInTail(Node{value: 2})
+	l.AddInTail(Node{value: 3})
+
+	find, err := l.Find(4)
+
+	assert.Error(t, err)
+	assert.Equal(t, Node{}, find)
+}
+
+func TestLinkedList_Find_SuccessFound(t *testing.T) {
+	l := LinkedList{}
+	l.AddInTail(Node{value: 1})
+	l.AddInTail(Node{value: 2})
+	l.AddInTail(Node{value: 3})
+
+	find, err := l.Find(3)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 3, find.value)
+	assert.Equal(t, *l.tail, find)
+}
+
+func TestLinkedList_FindAll_EmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	find := l.FindAll(3)
+
+	assert.Nil(t, find)
+}
+
+func TestLinkedList_FindAll_NotEmptyList(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 2}
+	n2 := Node{value: 1}
+	n3 := Node{value: 2}
+
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+	l.AddInTail(n3)
+
+	find := l.FindAll(2)
+
+	assert.NotNil(t, find)
+	assert.Len(t, find, 2)
+	assert.Equal(t, *l.head, find[0])
+	assert.Equal(t, *l.tail, find[1])
+}
+
+func TestLinkedList_DeleteFirst_EmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	l.Delete(2, false)
+
+	assert.Equal(t, 0, l.Count())
+}
+
+func TestLinkedList_DeleteFirst_OneElement(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 2}
+	l.AddInTail(n1)
+
+	l.Delete(2, false)
+
+	assert.Equal(t, 0, l.Count())
+	assert.Nil(t, l.head)
+	assert.Nil(t, l.tail)
+}
+
+func TestLinkedList_DeleteFirst_NotEmptyList_HeadElement(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 2}
+	n2 := Node{value: 1}
+	n3 := Node{value: 2}
+
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+	l.AddInTail(n3)
+
+	l.Delete(2, false)
+
+	assert.Equal(t, 2, l.Count())
+	assert.Equal(t, n3, *l.tail)
+	assert.Equal(t, 1, l.head.value)
+}
+
+func TestLinkedList_DeleteFirst_NotEmptyList_NotHeadElement(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 2}
+	n2 := Node{value: 1}
+	n3 := Node{value: 2}
+	n4 := Node{value: 1}
+	n5 := Node{value: 3}
+
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+	l.AddInTail(n3)
+	l.AddInTail(n4)
+	l.AddInTail(n5)
+
+	del := l.tail
+
+	l.Delete(3, false)
+
+	assert.Equal(t, 4, l.Count())
+	assert.Equal(t, n4, *l.tail)
+	assert.Equal(t, 2, l.head.value)
+	assert.True(t, l.tail != del)
+}
+
+func TestLinkedList_DeleteAll_EmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	l.Delete(2, true)
+
+	assert.Equal(t, 0, l.Count())
+}
+
+func TestLinkedList_DeleteAll_NotEmptyList_OneElement(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 1}
+
+	l.AddInTail(n1)
+
+	l.Delete(1, true)
+
+	assert.Equal(t, 0, l.Count())
+	assert.Nil(t, l.tail)
+	assert.Nil(t, l.head)
+}
+
+func TestLinkedList_DeleteAll_NotEmptyList_HeadElement(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 1}
+	n2 := Node{value: 2}
+
+	l.AddInTail(n1)
+	l.AddInTail(n1)
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+	l.AddInTail(n1)
+
+	l.Delete(1, true)
+
+	assert.Equal(t, 2, l.Count())
+	assert.Equal(t, 2, l.tail.value)
+	assert.Equal(t, 2, l.head.value)
+}
+
+func TestLinkedList_InsertFirst_EmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	n := Node{value: 1}
+	l.InsertFirst(n)
+
+	assert.Equal(t, n, *l.head)
+	assert.Equal(t, n, *l.tail)
+}
+
+func TestLinkedList_InsertFirst_NotEmptyList(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 1}
+	n2 := Node{value: 2}
+
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+	l.AddInTail(n1)
+
+	n3 := Node{value: 5}
+	l.InsertFirst(n3)
+
+	assert.Equal(t, 4, l.Count())
+	assert.Equal(t, 1, l.tail.value)
+	assert.Equal(t, 5, l.head.value)
+}
+
+func TestLinkedList_Insert_AfterHead(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 1}
+	n2 := Node{value: 2}
+
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+
+	n3 := Node{value: 5}
+	l.Insert(l.head, n3)
+
+	assert.Equal(t, 3, l.Count())
+	assert.Equal(t, 2, l.tail.value)
+	assert.Equal(t, 1, l.head.value)
+	assert.Equal(t, 5, l.head.next.value)
+}
+
+func TestLinkedList_Insert_AfterTail(t *testing.T) {
+	l := LinkedList{}
+	n1 := Node{value: 1}
+	n2 := Node{value: 2}
+
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+
+	n3 := Node{value: 5}
+	l.Insert(l.tail, n3)
+
+	assert.Equal(t, 3, l.Count())
+	assert.Equal(t, 5, l.tail.value)
+	assert.Equal(t, 1, l.head.value)
+}
+
+func TestLinkedList_Clean_EmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	l.Clean()
+
+	assert.Equal(t, 0, l.Count())
+	assert.Nil(t, l.tail)
+	assert.Nil(t, l.head)
+}
+
+func TestLinkedList_Clean_NotEmptyList(t *testing.T) {
+	l := LinkedList{}
+
+	n1 := Node{value: 1}
+	n2 := Node{value: 2}
+
+	l.AddInTail(n1)
+	l.AddInTail(n2)
+
+	l.Clean()
+
+	assert.Equal(t, 0, l.Count())
+	assert.Nil(t, l.tail)
+	assert.Nil(t, l.head)
+}
