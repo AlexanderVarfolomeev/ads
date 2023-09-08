ab1a9738f54c9f56e73ed1145ea13f1757f96950
a.o.varfolomeev <a.o.varfolomeev@tinkoff.ru>
2023-09-08 21:35:29 +0300

    fix sum

--- go/linkedList/additional.go
+++ go/linkedList/additional.go
@@ -3,14 +3,14 @@ package main
 import "errors"
 
 func Sum(l1 LinkedList, l2 LinkedList) (LinkedList, error) {
-	if l1.Count() != l2.Count() {
-		return LinkedList{}, errors.New("list sizes are not equal")
-	}
-
 	res := LinkedList{}
 	n1, n2 := l1.head, l2.head
 
-	for n1 != nil && n2 != nil {
+	for n1 != nil || n2 != nil {
+		if (n1 == nil && n2 != nil) || (n1 != nil && n2 == nil) {
+			return LinkedList{}, errors.New("list sizes are not equal")
+		}
+
 		res.AddInTail(Node{value: n1.value + n2.value})
 
 		n1 = n1.next
