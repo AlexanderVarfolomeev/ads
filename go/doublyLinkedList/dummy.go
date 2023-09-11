0333a7516e7a8eb60f9bc20a78d5e045394f79ea
AlexanderVarfolomeev <alexander.varfolomei@gmail.com>
2023-09-11 13:43:52 +0300

    Node as interface

--- go/doublyLinkedList/dummy.go
+++ go/doublyLinkedList/dummy.go
@@ -2,78 +2,132 @@ package main
 
 import "errors"
 
+type Node interface {
+	Next() Node
+	Prev() Node
+	SetNext(node Node)
+	SetPrev(node Node)
+	Value() int
+}
+
 type DummyNode struct {
-	prev  *DummyNode
-	next  *DummyNode
+	prev Node
+	next Node
+}
+
+func (n *DummyNode) Next() Node {
+	return n.next
+}
+
+func (n *DummyNode) Prev() Node {
+	return n.prev
+}
+
+func (n *DummyNode) SetNext(node Node) {
+	n.next = node
+}
+
+func (n *DummyNode) SetPrev(node Node) {
+	n.prev = node
+}
+
+func (n *DummyNode) Value() int {
+	return 0
+}
+
+type RealNode struct {
+	prev  Node
+	next  Node
 	value int
-	dummy bool
+}
+
+func (n *RealNode) Next() Node {
+	return n.next
+}
+
+func (n *RealNode) Prev() Node {
+	return n.prev
+}
+
+func (n *RealNode) SetNext(node Node) {
+	n.next = node
+}
+
+func (n *RealNode) SetPrev(node Node) {
+	n.prev = node
+}
+
+func (n *RealNode) Value() int {
+	return n.value
 }
 
 type DummyLinkedList2 struct {
-	head *DummyNode
-	tail *DummyNode
+	head Node
+	tail Node
 }
 
 func NewDummyList() *DummyLinkedList2 {
+	n := &DummyNode{}
 	l := &DummyLinkedList2{
-		head: &DummyNode{
-			dummy: true,
-		},
-		tail: &DummyNode{
-			dummy: true,
-		},
+		head: n,
+		tail: n,
 	}
 
 	l.Clean()
 	return l
 }
 
-func (l *DummyLinkedList2) Head() *DummyNode {
-	if l.head.next.dummy {
+func isDummyNode(n Node) bool {
+	_, ok := n.(*DummyNode)
+	return ok
+}
+
+func (l *DummyLinkedList2) Head() Node {
+	if isDummyNode(l.head.Next()) {
 		return nil
 	}
 
-	return l.head.next
+	return l.head.Next()
 }
 
-func (l *DummyLinkedList2) Tail() *DummyNode {
-	if l.tail.prev.dummy {
+func (l *DummyLinkedList2) Tail() Node {
+	if isDummyNode(l.tail.Prev()) {
 		return nil
 	}
 
-	return l.tail.prev
+	return l.tail.Prev()
 }
 
-func (l *DummyLinkedList2) AddInTail(item DummyNode) {
-	addAfter(l.tail.prev, &item)
+func (l *DummyLinkedList2) AddInTail(item Node) {
+	addAfter(l.tail.Prev(), item)
 }
 
 func (l *DummyLinkedList2) Count() int {
 	count := 0
 
-	for node := l.head.next; !node.dummy; node = node.next {
+	for node := l.head.Next(); !isDummyNode(node); node = node.Next() {
 		count++
 	}
 
 	return count
 }
 
-func (l *DummyLinkedList2) Find(n int) (DummyNode, error) {
-	for node := l.head.next; !node.dummy; node = node.next {
-		if node.value == n {
-			return *node, nil
+func (l *DummyLinkedList2) Find(n int) (Node, error) {
+	for node := l.head.Next(); !isDummyNode(node); node = node.Next() {
+		if node.Value() == n {
+			return node, nil
 		}
 	}
 
-	return DummyNode{value: 0, next: nil}, errors.New("node not found")
+	return nil, errors.New("node not found")
 }
 
-func (l *DummyLinkedList2) FindAll(n int) []DummyNode {
-	var nodes []DummyNode
+func (l *DummyLinkedList2) FindAll(n int) []Node {
+	var nodes []Node
 
-	for node := l.head.next; !node.dummy; node = node.next {
-		if node.value == n {
-			nodes = append(nodes, *node)
+	for node := l.head.Next(); !isDummyNode(node); node = node.Next() {
+		if node.Value() == n {
+			nodes = append(nodes, node)
 		}
 	}
 
@@ -83,11 +137,10 @@ func (l *DummyLinkedList2) FindAll(n int) []DummyNode {
 func (l *DummyLinkedList2) Delete(n int, all bool) {
 	deleted := false
 
-	for node := l.head.next; !node.dummy; node = node.next {
-		if node.value == n {
-			node.prev.next = node.next
-			node.next.prev = node.prev
-
+	for node := l.head.Next(); !isDummyNode(node); node = (node).Next() {
+		if node.Value() == n {
+			node.Prev().SetNext(node.Next())
+			node.Next().SetPrev(node.Prev())
 			deleted = true
 		}
 
@@ -97,28 +150,23 @@ func (l *DummyLinkedList2) Delete(n int, all bool) {
 	}
 }
 
-func (l *DummyLinkedList2) Insert(after *DummyNode, add DummyNode) {
-	for node := l.head.next; !node.dummy; node = node.next {
-		if node == after {
-			addAfter(after, &add)
-			return
-		}
-	}
+func (l *DummyLinkedList2) Insert(after Node, add Node) {
+	addAfter(after, add)
 }
 
-func (l *DummyLinkedList2) InsertFirst(first DummyNode) {
-	addAfter(l.head, &first)
+func (l *DummyLinkedList2) InsertFirst(first Node) {
+	addAfter(l.head, first)
 }
 
 func (l *DummyLinkedList2) Clean() {
-	l.head.next = l.tail
-	l.tail.prev = l.head
+	l.head.SetNext(l.head)
+	l.head.SetPrev(l.head)
 }
 
-func addAfter(after *DummyNode, add *DummyNode) {
-	add.next = after.next
-	add.prev = after
+func addAfter(after Node, add Node) {
+	add.SetNext(after.Next())
+	add.SetPrev(after)
 
-	after.next.prev = add
-	after.next = add
+	after.Next().SetPrev(add)
+	after.SetNext(add)
 }
--- go/doublyLinkedList/dummy_test.go
+++ go/doublyLinkedList/dummy_test.go
@@ -8,25 +8,30 @@ import (
 
 func TestDummyLinkedList2_AddInTail_EmptyList(t *testing.T) {
 	l := NewDummyList()
-
-	l.AddInTail(DummyNode{value: 12})
+	l.AddInTail(&RealNode{
+		prev:  nil,
+		next:  nil,
+		value: 12,
+	})
 
 	assert.NotNil(t, l.Tail())
 	assert.NotNil(t, l.Head())
 	assert.Equal(t, 1, l.Count())
-	assert.Equal(t, 12, l.Head().value)
-	assert.Equal(t, 12, l.Tail().value)
+	assert.Equal(t, 12, l.Head().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_AddInTail_NotEmptyList(t *testing.T) {
 	l := NewDummyList()
-	l.AddInTail(DummyNode{value: 1})
-	l.AddInTail(DummyNode{value: 2})
-	l.AddInTail(DummyNode{value: 12})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 12})
 
 	assert.Equal(t, 3, l.Count())
-	assert.Equal(t, 1, l.Head().value)
-	assert.Equal(t, 12, l.Tail().value)
+
+	assert.Equal(t, 1, l.Head().Value())
+	assert.Equal(t, 12, l.Tail().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Count_EmptyList(t *testing.T) {
@@ -34,41 +39,45 @@ func TestDummyLinkedList2_Count_EmptyList(t *testing.T) {
 	c := l.Count()
 
 	assert.Equal(t, 0, c)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Count_NotEmptyList(t *testing.T) {
 	l := NewDummyList()
-	l.AddInTail(DummyNode{value: 1})
-	l.AddInTail(DummyNode{value: 2})
-	l.AddInTail(DummyNode{value: 3})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 3})
 
 	c := l.Count()
 
 	assert.Equal(t, 3, c)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Count_NotEmptyListAfterDeleteElement(t *testing.T) {
 	l := NewDummyList()
-	l.AddInTail(DummyNode{value: 1})
-	l.AddInTail(DummyNode{value: 2})
-	l.AddInTail(DummyNode{value: 3})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 3})
 
 	l.Delete(2, false)
 	c := l.Count()
 
 	assert.Equal(t, 2, c)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Count_EmptyListAfterDeleteAllElements(t *testing.T) {
 	l := NewDummyList()
-	l.AddInTail(DummyNode{value: 2})
-	l.AddInTail(DummyNode{value: 2})
-	l.AddInTail(DummyNode{value: 2})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 2})
 
 	l.Delete(2, true)
 	c := l.Count()
 
 	assert.Equal(t, 0, c)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Find_EmptyList(t *testing.T) {
@@ -77,32 +86,35 @@ func TestDummyLinkedList2_Find_EmptyList(t *testing.T) {
 	find, err := l.Find(2)
 
 	assert.Error(t, err)
-	assert.Equal(t, DummyNode{}, find)
+	assert.Nil(t, find)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Find_NotFound(t *testing.T) {
 	l := NewDummyList()
-	l.AddInTail(DummyNode{value: 1})
-	l.AddInTail(DummyNode{value: 2})
-	l.AddInTail(DummyNode{value: 3})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 3})
 
 	find, err := l.Find(4)
 
 	assert.Error(t, err)
-	assert.Equal(t, DummyNode{}, find)
+	assert.Nil(t, find)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Find_SuccessFound(t *testing.T) {
 	l := NewDummyList()
-	l.AddInTail(DummyNode{value: 1})
-	l.AddInTail(DummyNode{value: 2})
-	l.AddInTail(DummyNode{value: 3})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 3})
 
 	find, err := l.Find(3)
 
 	assert.NoError(t, err)
-	assert.Equal(t, 3, find.value)
-	assert.Equal(t, *l.Tail(), find)
+	assert.Equal(t, 3, find.Value())
+	assert.Equal(t, l.Tail(), find)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_FindAll_EmptyList(t *testing.T) {
@@ -111,13 +123,14 @@ func TestDummyLinkedList2_FindAll_EmptyList(t *testing.T) {
 	find := l.FindAll(3)
 
 	assert.Nil(t, find)
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_FindAll_NotEmptyList(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 2}
-	n2 := DummyNode{value: 1}
-	n3 := DummyNode{value: 2}
+	n1 := &RealNode{value: 2}
+	n2 := &RealNode{value: 1}
+	n3 := &RealNode{value: 2}
 
 	l.AddInTail(n1)
 	l.AddInTail(n2)
@@ -127,8 +140,9 @@ func TestDummyLinkedList2_FindAll_NotEmptyList(t *testing.T) {
 
 	assert.NotNil(t, find)
 	assert.Len(t, find, 2)
-	assert.Equal(t, *l.Head(), find[0])
-	assert.Equal(t, *l.Tail(), find[1])
+	assert.Equal(t, l.Head(), find[0])
+	assert.Equal(t, l.Tail(), find[1])
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteFirst_EmptyList(t *testing.T) {
@@ -139,11 +153,12 @@ func TestDummyLinkedList2_DeleteFirst_EmptyList(t *testing.T) {
 	assert.Equal(t, 0, l.Count())
 	assert.Nil(t, l.Head())
 	assert.Nil(t, l.Tail())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteFirst_OneElement(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 2}
+	n1 := &RealNode{value: 2}
 	l.AddInTail(n1)
 
 	l.Delete(2, false)
@@ -151,13 +166,14 @@ func TestDummyLinkedList2_DeleteFirst_OneElement(t *testing.T) {
 	assert.Equal(t, 0, l.Count())
 	assert.Nil(t, l.Head())
 	assert.Nil(t, l.Tail())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteFirst_NotEmptyList_HeadElement(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 2}
-	n2 := DummyNode{value: 1}
-	n3 := DummyNode{value: 2}
+	n1 := &RealNode{value: 2}
+	n2 := &RealNode{value: 1}
+	n3 := &RealNode{value: 2}
 
 	l.AddInTail(n1)
 	l.AddInTail(n2)
@@ -166,19 +182,23 @@ func TestDummyLinkedList2_DeleteFirst_NotEmptyList_HeadElement(t *testing.T) {
 	l.Delete(2, false)
 
 	assert.Equal(t, 2, l.Count())
-	assert.Equal(t, 1, l.Head().value)
-	assert.Equal(t, 2, l.Head().next.value)
-	assert.Equal(t, 2, l.Tail().value)
-	assert.Equal(t, 1, l.Tail().prev.value)
+	assert.Equal(t, 1, l.Head().Value())
+
+	assert.Equal(t, 2, l.Head().Next().Value())
+
+	assert.Equal(t, 2, l.Tail().Value())
+
+	assert.Equal(t, 1, l.Tail().Prev().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteFirst_NotEmptyList_NotHeadElement(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 2}
-	n2 := DummyNode{value: 1}
-	n3 := DummyNode{value: 2}
-	n4 := DummyNode{value: 1}
-	n5 := DummyNode{value: 3}
+	n1 := &RealNode{value: 2}
+	n2 := &RealNode{value: 1}
+	n3 := &RealNode{value: 2}
+	n4 := &RealNode{value: 1}
+	n5 := &RealNode{value: 3}
 
 	l.AddInTail(n1)
 	l.AddInTail(n2)
@@ -186,33 +206,32 @@ func TestDummyLinkedList2_DeleteFirst_NotEmptyList_NotHeadElement(t *testing.T) 
 	l.AddInTail(n4)
 	l.AddInTail(n5)
 
-	del := l.Tail()
-
 	l.Delete(3, false)
 
 	assert.Equal(t, 4, l.Count())
-	assert.Equal(t, n4.value, l.Tail().value)
-	assert.Equal(t, 2, l.Head().value)
-	assert.True(t, l.Tail() != del)
+
+	assert.Equal(t, n4.value, l.Tail().Value())
+
+	assert.Equal(t, 2, l.Head().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteAll_NotEmptyList_SameRow(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
-	n2 := DummyNode{value: 2}
-	n3 := DummyNode{value: 3}
 
-	l.AddInTail(n1)
-	l.AddInTail(n2)
-	l.AddInTail(n2)
-	l.AddInTail(n2)
-	l.AddInTail(n2)
-	l.AddInTail(n3)
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 3})
 
 	l.Delete(2, true)
 
 	assert.Equal(t, 2, l.Count())
-	assert.Equal(t, 1, l.Head().value)
+
+	assert.Equal(t, 1, l.Head().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteAll_EmptyList(t *testing.T) {
@@ -223,11 +242,12 @@ func TestDummyLinkedList2_DeleteAll_EmptyList(t *testing.T) {
 	assert.Equal(t, 0, l.Count())
 	assert.Nil(t, l.Tail())
 	assert.Nil(t, l.Head())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteAll_NotEmptyList_OneElement(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
+	n1 := &RealNode{value: 1}
 
 	l.AddInTail(n1)
 
@@ -236,124 +256,130 @@ func TestDummyLinkedList2_DeleteAll_NotEmptyList_OneElement(t *testing.T) {
 	assert.Equal(t, 0, l.Count())
 	assert.Nil(t, l.Tail())
 	assert.Nil(t, l.Head())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteAll_NotEmptyList_AllElements(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
 
-	l.AddInTail(n1)
-	l.AddInTail(n1)
-	l.AddInTail(n1)
-	l.AddInTail(n1)
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 1})
 
 	l.Delete(1, true)
 
 	assert.Equal(t, 0, l.Count())
 	assert.Nil(t, l.Tail())
 	assert.Nil(t, l.Head())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_DeleteAll_NotEmptyList_HeadElement(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
-	n2 := DummyNode{value: 2}
-
-	l.AddInTail(n1)
-	l.AddInTail(n1)
-	l.AddInTail(n1)
-	l.AddInTail(n2)
-	l.AddInTail(n1)
-	l.AddInTail(n2)
-	l.AddInTail(n1)
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 1})
 
 	l.Delete(1, true)
 
 	assert.Equal(t, 2, l.Count())
-	assert.Equal(t, 2, l.Tail().value)
-	assert.Equal(t, 2, l.Head().value)
+	assert.Equal(t, l.head, l.tail)
+	assert.Equal(t, 2, l.Tail().Value())
+	assert.Equal(t, 2, l.Head().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_InsertFirst_EmptyList(t *testing.T) {
 	l := NewDummyList()
 
-	n := DummyNode{value: 1}
-	l.InsertFirst(n)
+	l.InsertFirst(&RealNode{value: 1})
 	n1 := l.Head()
 	n2 := l.Tail()
 
 	assert.Equal(t, n1, n2)
 	assert.Equal(t, l.Head(), l.Tail())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_InsertFirst_NotEmptyList(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
-	n2 := DummyNode{value: 2}
 
-	l.AddInTail(n1)
-	l.AddInTail(n2)
-	l.AddInTail(n1)
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 3})
+	l.AddInTail(&RealNode{value: 1})
 
-	n3 := DummyNode{value: 5}
-	l.InsertFirst(n3)
+	l.InsertFirst(&RealNode{value: 5})
 
 	assert.Equal(t, 4, l.Count())
-	assert.Equal(t, 1, l.Tail().value)
-	assert.Equal(t, 5, l.Head().value)
+
+	assert.Equal(t, 1, l.Tail().Value())
+
+	assert.Equal(t, 5, l.Head().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Insert_InMiddle(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
-	n2 := DummyNode{value: 2}
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 1})
+	l.AddInTail(&RealNode{value: 2})
+	l.AddInTail(&RealNode{value: 2})
 
-	l.AddInTail(n1)
-	l.AddInTail(n1)
-	l.AddInTail(n2)
-	l.AddInTail(n2)
-
-	n3 := DummyNode{value: 5}
-	l.Insert(l.Head().next, n3)
+	l.Insert(l.Head().Next(), &RealNode{value: 5})
 
 	assert.Equal(t, 5, l.Count())
-	assert.Equal(t, 2, l.Tail().value)
-	assert.Equal(t, 1, l.Head().value)
-	assert.Equal(t, 5, l.Head().next.next.value)
-	assert.Equal(t, 5, l.Tail().prev.prev.value)
+
+	assert.Equal(t, 2, l.Tail().Value())
+
+	assert.Equal(t, 1, l.Head().Value())
+
+	assert.Equal(t, 5, l.Head().Next().Next().Value())
+
+	assert.Equal(t, 5, l.Tail().Prev().Prev().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Insert_AfterHead(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
-	n2 := DummyNode{value: 2}
+	n1 := &RealNode{value: 1}
+	n2 := &RealNode{value: 2}
 
 	l.AddInTail(n1)
 	l.AddInTail(n2)
 
-	n3 := DummyNode{value: 5}
+	n3 := &RealNode{value: 5}
 	l.Insert(l.Head(), n3)
 
 	assert.Equal(t, 3, l.Count())
-	assert.Equal(t, 2, l.Tail().value)
-	assert.Equal(t, 1, l.Head().value)
-	assert.Equal(t, 5, l.Head().next.value)
+
+	assert.Equal(t, 2, l.Tail().Value())
+
+	assert.Equal(t, 1, l.Head().Value())
+
+	assert.Equal(t, 5, l.Head().Next().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Insert_AfterTail(t *testing.T) {
 	l := NewDummyList()
-	n1 := DummyNode{value: 1}
-	n2 := DummyNode{value: 2}
+	n1 := &RealNode{value: 1}
+	n2 := &RealNode{value: 2}
 
 	l.AddInTail(n1)
 	l.AddInTail(n2)
 
-	n3 := DummyNode{value: 5}
+	n3 := &RealNode{value: 5}
 	l.Insert(l.Tail(), n3)
 
 	assert.Equal(t, 3, l.Count())
-	assert.Equal(t, 5, l.Tail().value)
-	assert.Equal(t, 1, l.Head().value)
+	assert.Equal(t, 5, l.Tail().Value())
+	assert.Equal(t, 2, l.Head().Next().Value())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Clean_EmptyList(t *testing.T) {
@@ -364,13 +390,14 @@ func TestDummyLinkedList2_Clean_EmptyList(t *testing.T) {
 	assert.Equal(t, 0, l.Count())
 	assert.Nil(t, l.Tail())
 	assert.Nil(t, l.Head())
+	assert.Equal(t, l.head, l.tail)
 }
 
 func TestDummyLinkedList2_Clean_NotEmptyList(t *testing.T) {
 	l := NewDummyList()
 
-	n1 := DummyNode{value: 1}
-	n2 := DummyNode{value: 2}
+	n1 := &RealNode{value: 1}
+	n2 := &RealNode{value: 2}
 
 	l.AddInTail(n1)
 	l.AddInTail(n2)
@@ -382,4 +409,5 @@ func TestDummyLinkedList2_Clean_NotEmptyList(t *testing.T) {
 	assert.Equal(t, 0, l.Count())
 	assert.Nil(t, l.Tail())
 	assert.Nil(t, l.Head())
+	assert.Equal(t, l.head, l.tail)
 }
