623f13173318c1e44d07ce023dfde0379b780fd4
a.o.varfolomeev <a.o.varfolomeev@tinkoff.ru>
2023-11-26 17:51:58 +0300

    fix 7

--- go/recursion/secondMax/main.go
+++ go/recursion/secondMax/main.go
@@ -1,46 +1,48 @@
 package main
 
-func SecondMax(list []int) int {
-	return secondMax(list, 0, 0, false, false)
+import "errors"
+
+func SecondMax(list []int) (int, error) {
+	if len(list) < 2 {
+		return 0, errors.New("list contains less than 2 items")
+	}
+	m1 := Max(list[0], list[1])
+	m2 := Min(list[0], list[1])
+
+	return secondMax(list[2:], m1, m2), nil
 }
 
-func secondMax(list []int, m1, m2 int, found1, found2 bool) int {
+func secondMax(list []int, m1, m2 int) int {
 	if len(list) == 0 {
-		if !found1 && !found2 {
-			return 0
-		}
-
-		if !found2 {
-			return m1
-		}
-
 		return m2
 	}
+	val := list[0]
 
-	if !found1 {
-		m1 = list[0]
-		found1 = true
-		return secondMax(list[1:], m1, m2, found1, found2)
-	}
-
-	if !found2 {
-		m2 = list[0]
-		found2 = true
-		if m2 > m1 {
-			m1, m2 = m2, m1
-		}
-		return secondMax(list[1:], m1, m2, found1, found2)
-	}
-
-	if list[0] > m1 {
+	if val > m1 {
 		m2 = m1
-		m1 = list[0]
-	} else if list[0] > m2 {
-		m2 = list[0]
+		m1 = m2
+	} else if val > m2 {
+		m2 = val
 	}
 
-	return secondMax(list[1:], m1, m2, found1, found2)
+	return secondMax(list[1:], m1, m2)
 }
 
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
 func main() {
+
 }
--- go/recursion/secondMax/main_test.go
+++ go/recursion/secondMax/main_test.go
@@ -7,15 +7,19 @@ import (
 )
 
 func TestSecondMax(t *testing.T) {
-	secondMax := SecondMax([]int{2, 5, 4, 3, 5})
+	secondMax, err := SecondMax([]int{2, 5, 4, 3, 5})
 	assert.Equal(t, 5, secondMax)
+	assert.NoError(t, err)
 
-	secondMax = SecondMax([]int{2, 5, 4, 3})
+	secondMax, err = SecondMax([]int{2, 5, 4, 3})
 	assert.Equal(t, 4, secondMax)
+	assert.NoError(t, err)
 
-	secondMax = SecondMax([]int{2})
-	assert.Equal(t, 2, secondMax)
-
-	secondMax = SecondMax([]int{})
+	secondMax, err = SecondMax([]int{2})
 	assert.Equal(t, 0, secondMax)
+	assert.Error(t, err)
+
+	secondMax, err = SecondMax([]int{})
+	assert.Equal(t, 0, secondMax)
+	assert.Error(t, err)
 }
